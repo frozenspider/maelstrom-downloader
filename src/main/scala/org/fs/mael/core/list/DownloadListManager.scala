7441791f3b34f96a208fb707e3bebf0ed83a40ad
Alex Abdugafarov <fswork90@gmail.com>
2018-02-19 06:35:20 +0500

    Simple implementation for DownloadListManager

---
+++ src/main/scala/org/fs/mael/core/list/DownloadListManager.scala
@@ -0,0 +1,34 @@
+package org.fs.mael.core.list
+
+import java.util.UUID
+
+import org.fs.mael.core.entry.DownloadEntry
+import org.fs.mael.core.event.EventManager
+
+/*
+ * TODO:
+ * Persist
+ */
+object DownloadListManager {
+  private var entries: Set[DownloadEntry] = Set.empty
+
+  /** Add a new entry to a download list, firing an event */
+  def add(de: DownloadEntry): Unit = {
+    this.synchronized {
+      entries += de
+      EventManager.fireAdded(de)
+    }
+  }
+
+  /** Remove an existing entry from a download list, firing an event */
+  def remove(deId: UUID): Unit = {
+    this.synchronized {
+      entries = entries filter (_.id != deId)
+      EventManager.fireRemoved(deId)
+    }
+  }
+
+  def list(): Set[DownloadEntry] = {
+    entries
+  }
+}
