a635cc2109c53cf58d86ceae2fd4ad60abab7568
Alex Abdugafarov <fswork90@gmail.com>
2018-03-09 21:57:40 +0500

    #3: Equality and serialization of HttpEntryData

--- src/main/scala/org/fs/mael/backend/http/HttpDataSerializer.scala
+++ src/main/scala/org/fs/mael/backend/http/HttpDataSerializer.scala
@@ -2,14 +2,24 @@ package org.fs.mael.backend.http
 
 import org.fs.mael.core.backend.BackendDataSerializer
 import org.json4s._
+import org.json4s.JsonDSL._
+import org.json4s.jackson.Serialization
 
 class HttpDataSerializer extends BackendDataSerializer[HttpEntryData] {
+  implicit private val formats = Serialization.formats(NoTypeHints)
+
   // FIXME
-  override def serializeFields(bsed: HttpEntryData): Seq[JField] = {
-    Seq.empty
+  override def serializeFields(ed: HttpEntryData): Seq[JField] = {
+    val jObj: JObject = (
+      ("userAgentOption" -> ed.userAgentOption)
+    )
+    jObj.obj
   }
 
   override def deserialize(jObj: JObject): HttpEntryData = {
-    new HttpEntryData
+    val ed = new HttpEntryData
+    ed.userAgentOption = (jObj \ "userAgentOption").extractOpt[String]
+    ed
   }
+
 }
--- src/main/scala/org/fs/mael/backend/http/HttpEntryData.scala
+++ src/main/scala/org/fs/mael/backend/http/HttpEntryData.scala
@@ -1,5 +1,7 @@
 package org.fs.mael.backend.http
 
+import org.apache.commons.lang3.builder.EqualsBuilder
+import org.apache.commons.lang3.builder.HashCodeBuilder
 import org.fs.mael.core.entry.BackendSpecificEntryData
 
 class HttpEntryData extends BackendSpecificEntryData {
@@ -9,9 +11,15 @@ class HttpEntryData extends BackendSpecificEntryData {
   var userAgentOption: Option[String] = None
 
   override def equalsInner(that: BackendSpecificEntryData): Boolean = that match {
-    case that: HttpEntryData => true
-    case _                   => false
+    case that: HttpEntryData =>
+      (new EqualsBuilder)
+        .append(this.userAgentOption, that.userAgentOption)
+        .build()
+    case _ => false
   }
 
-  override def hashCodeInner: Int = 31
+  override def hashCodeInner: Int =
+    (new HashCodeBuilder)
+      .append(this.userAgentOption)
+      .toHashCode
 }
